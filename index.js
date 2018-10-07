20da86f88b42113b1152151d5ecd0d01656dcf58
Puru <mpurusottamc@gmail.com>
2018-10-07 15:25:55 -0700

    added semicolon

--- index.js
+++ index.js
@@ -8,31 +8,31 @@ import CloudStorage from './lib/cloud-storage.js';
 console.log(`Configuration: ${JSON.stringify(config)}`);
 
 function main() {
-    const url = 'www.cloudanix.com';
-    const url2png = new URL2PNG(config);
+	const url = 'www.cloudanix.com';
+	const url2png = new URL2PNG(config);
 
-    const cloudStorage = new CloudStorage(config.storage.auth);
+	const cloudStorage = new CloudStorage(config.storage.auth);
 
-    url2png.generateScreenshot(url)
-        .then(result => {
-            console.log(`url2png success: ${result}`);
+	url2png.generateScreenshot(url)
+		.then(result => {
+			console.log(`url2png success: ${result}`);
 
-            return cloudStorage.uploadFile(result, config.storage.options);
-        })
-        .then(filePath => {
-            console.log(`upload file success: ${filePath}`);
+			return cloudStorage.uploadFile(result, config.storage.options);
+		})
+		.then(filePath => {
+			console.log(`upload file success: ${filePath}`);
 
-            fs.unlink(filePath, (error) => {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log(`file deleted: ${filePath}`);
-                }
-            })
-        })
-        .catch(error => {
-            console.log(`failed to generate screenshot from url2png and upload to google storage for ${url} : ${error}`);
-        });
+			fs.unlink(filePath, (error) => {
+				if (error) {
+					console.log(error);
+				} else {
+					console.log(`file deleted: ${filePath}`);
+				}
+			});
+		})
+		.catch(error => {
+			console.log(`failed to generate screenshot from url2png and upload to google storage for ${url} : ${error}`);
+		});
 }
 
 main();
